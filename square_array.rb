04ca6c2f807b4d669c9b8524cecb09482170251c
Emma Konrad <emma.konrad92@gmail.com>
2017-10-03 17:29:28 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,5 @@
-#def square_array(array)
-#  new_array = []
-#  array.each { |numbers| new_array << numbers ** 2 }
-#  return new_array
-#end
-
 def square_array(array)
   new_array = []
-  array.collect { |numbers| numbers ** 2}
-  puts new_array
+  array.each { |numbers| new_array << numbers ** 2 }
+  return new_array
 end
